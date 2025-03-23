887098e2de4d48e89c7ab2da4c648516201d04a9
dreadnokkk <pedroaiello.delazari1@gmail.com>
2025-03-23 07:05:04 +0000

    new codee

--- xadrez.c
+++ xadrez.c
@@ -11,7 +11,7 @@ int populacao;
 float area, pib;
 int turisticos;
 float densidadepoulacional;
-float pibpercapita;
+float pibpe;
 
 //infomações fornecidas
 
@@ -47,7 +47,7 @@ densidadepoulacional = (float) populacao / area;
 
 //calculo do pib per capita
 
-pibpercapita =  pib / populacao;
+pibpe = (float) ( pib / populacao ) * 1000000000;
 
 printf("***Dados da Primeira Carta***\n");
 printf("Estado: %c\n", estado);
@@ -57,63 +57,7 @@ printf("População: %d\n", populacao);
 printf("Área : %.2f km²\n", area);
 printf("PIB : %.2f bilhões de reais\n", pib);
 printf("Pontos turísticos : %d\n", turisticos);
-printf("Densidade populacional : %.2f hab/km²\n", populacao / area, (populacao / area));
-printf("Pib per capita : %.2f reais\n", pib / populacao, (pib / populacao));
+printf("Densidade populacional : %.2f hab/km²\n", densidadepoulacional);
+printf("Pib per capita : %.2f reais\n", pibpe);
 
-
-char estado;
-char codigo [20];
-char nomedacidade [50];
-int populacao;
-float area, pib;
-int turisticos;
-float densidadepoulacional;
-float pibpercapita;
-
-//infomações fornecidas
-
-printf("Digite o estado a seguir: ");
-scanf("%c", &estado);
-
-printf("Digite o código da sua primeira carta:  ");
-scanf("%s", &codigo);
-
-printf("Digite o nome da sua primeira carta:  ");
-scanf("%s", &nomedacidade);
-
-
-printf("Digite a população da sua primeira carta seguir:  ");
-scanf("%d", &populacao);
-
-
-printf("Digite a área da sua primeira carta a seguir:  ");
-scanf("%f", &area);
-
-
-printf("Digite o PIB da sua primeira carta a seguir:  ");
-scanf("%f", &pib);
-
-
-printf("Digite os pontos turíticos da sua primeira carta a seguir:  ");
-scanf("%d", &turisticos);
-
-
-//calculo da densidade populacional 
-
-densidadepoulacional = (float) populacao / area;
-
-//calculo do pib per capita
-
-pibpercapita =  pib / populacao;
-
-printf("***Dados da Primeira Carta***\n");
-printf("Estado: %c\n", estado);
-printf("Código: %s\n", codigo);
-printf("Nome da cidade : %s\n", nomedacidade);
-printf("População: %d\n", populacao);
-printf("Área : %.2f km²\n", area);
-printf("PIB : %.2f bilhões de reais\n", pib);
-printf("Pontos turísticos : %d\n", turisticos);
-printf("Densidade populacional : %.2f hab/km²\n", populacao / area, (populacao / area));
-printf("Pib per capita : %.2f reais\n", pib / populacao, (pib / populacao));
 }
